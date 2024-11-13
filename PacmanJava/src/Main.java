3d140a6c58df793f7d5a8ff3b14786db3282067f
kutay <ibrahimkutaysahin765@gmail.com>
2024-11-13 18:34:52 +0300

    .

--- PacmanJava/src/Main.java
+++ PacmanJava/src/Main.java
@@ -1,8 +1,26 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int rowSize = 21;
+		int columnSize = 19;
+		int tileSize = 32;
+		int boardWidth = columnSize * tileSize;
+		int boardHeight = rowSize * tileSize;
+		
+		
+		JFrame frame = new JFrame("PacMan");
+		frame.setSize(boardWidth, boardHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		Pacman pacman = new Pacman();
+		frame.add(pacman);
+		frame.pack();
+		frame.setVisible(true);
+		
 
 	}
 
--- PacmanJava/src/Pacman.java
+++ PacmanJava/src/Pacman.java
@@ -6,119 +6,58 @@ import javax.swing.*;
 
 
 
-public class Pacman extends JPanel implements ActionListener, KeyListener {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+public class Pacman extends JPanel  {
+	
+	private int rowSize = 21;
+	private int columnSize = 19;
+	private int tileSize = 32;
+	private int boardWidth = columnSize * tileSize;
+	private int boardHeight = rowSize * tileSize;
+	
+	private Image wallImage;
+	private Image blueGhostImage;
+	private Image redGhostImage;
+	private Image pinkGhostImage;
+	private Image orangeGhostImage;
+	
+	private Image pacmanUpImage;
+	private Image pacmanDownImage;
+	private Image pacmanLeftImage;
+	private Image pacmanRightImage;
+	
+//	private Image scaredGhostImage;
+//	
+//	private Image cherryImage;
+//	private Image cherry2Image;
+//	private Image powerFoodImage;
+	
+	
+	
+	
+	public Pacman() {
+		setPreferredSize(new Dimension(boardWidth,boardHeight));
+		setBackground(Color.black);
+		
+		// upload image
+		
+		// wall image
+		wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+		
+		// ghost image
+		blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+		redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+		pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+		orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+		
+		// pacman image
+		pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+		pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
+		pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+		pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+	}
 	
 	
 	
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 
 }
